2608193193a1bf193cef5a6ddd9d94f155ddc552
dharamsingh <jsracer71@gmail.com>
2020-06-05 12:47:29 -0400

    new code

---
+++ file2.js
@@ -0,0 +1,10 @@
+    var sum = 0;
+    
+      for (var i=1000; i > 17; i--) {
+            sum += i;
+          i -= 11;
+      }
+      
+      console.log(sum)
+  
+  
--- happy.js
+++ happy.js
@@ -2,8 +2,20 @@
 // REPORT on the SUM of all numbers from 1000 going down to 17Count 
 //down by 11 each timeADD UP all the numbers you step over 
 //as you are counting down from 1000, going down 11 each time,
-// until you get to 17Output the result
+// until you get to 17 Output the result
+
+
+function a(){
+    let sum = 0
 
 for ( i=0;i<5;i++){
-    console.log("hao")
+    console.log("Hello")
+    sum += i
+
+    if (i==5){
+        break
+    }
 }
+return sum
+}
+console.log("sum" + a())
