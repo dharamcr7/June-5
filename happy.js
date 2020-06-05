db8bcb008ba71bc7fa76e0b2364c390d1d626861
dharamsingh <jsracer71@gmail.com>
2020-06-05 11:05:39 -0400

    hl

---
+++ happy.js
@@ -0,0 +1,9 @@
+
+// REPORT on the SUM of all numbers from 1000 going down to 17Count 
+//down by 11 each timeADD UP all the numbers you step over 
+//as you are counting down from 1000, going down 11 each time,
+// until you get to 17Output the result
+
+for ( i=0;i<5;i++){
+    console.log("hao")
+}
