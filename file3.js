493247781a0a37880ec892aee89ea88d3359d4ee
dharamsingh <jsracer71@gmail.com>
2020-06-08 16:06:43 -0400

    file 3

---
+++ file3.js
@@ -0,0 +1,19 @@
+
+// REPORT on the SUM of all numbers from 100 going down to 5 minus 7 each time 
+
+
+
+function a(){
+    let sum = 0
+    let finalsum=0
+
+for ( i=100;i>=5;i--){
+
+    sum+=i
+    i -= 7
+    console.log(sum)
+   }
+ 
+}
+
+a()
